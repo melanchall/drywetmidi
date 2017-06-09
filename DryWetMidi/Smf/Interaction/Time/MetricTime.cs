b373f13726a28533592762ae22fd12dc7ccf99c4
Melanchall <m.hall.muz@gmail.com>
2017-06-09 18:42:37 +0700

    Add conversion between MIDI time and metric time (hours, minutes, seconds)

---
+++ DryWetMidi/Smf/Interaction/Time/MetricTime.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace Melanchall.DryWetMidi.Smf.Interaction
+{
+    public sealed class MetricTime
+    {
+        #region Constants
+
+        private const int MicrosecondsInMillisecond = 1000;
+        private const long TicksInMicrosecond = TimeSpan.TicksPerMillisecond / MicrosecondsInMillisecond;
+
+        #endregion
+
+        #region Fields
+
+        private readonly TimeSpan _timeSpan;
+
+        #endregion
+
+        #region Constructor
+
+        public MetricTime(long totalMicroseconds)
+        {
+            if (totalMicroseconds < 0)
+                throw new ArgumentOutOfRangeException(nameof(totalMicroseconds),
+                                                      totalMicroseconds,
+                                                      "Number of microseconds is negative.");
+
+            _timeSpan = new TimeSpan(totalMicroseconds * TicksInMicrosecond);
+        }
+
+        public MetricTime(int hours, int minutes, int seconds)
+        {
+            if (hours < 0)
+                throw new ArgumentOutOfRangeException(nameof(hours), hours, "Number of hours is negative.");
+
+            if (minutes < 0)
+                throw new ArgumentOutOfRangeException(nameof(minutes), minutes, "Number of minutes is negative.");
+
+            if (seconds < 0)
+                throw new ArgumentOutOfRangeException(nameof(seconds), seconds, "Number of seconds is negative.");
+
+            _timeSpan = new TimeSpan(hours, minutes, seconds);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public long TotalMicroseconds => _timeSpan.Ticks / TicksInMicrosecond;
+
+        public int Hours => _timeSpan.Hours;
+
+        public int Minutes => _timeSpan.Minutes;
+
+        public int Seconds => _timeSpan.Seconds;
+
+        public int Milliseconds => _timeSpan.Milliseconds;
+
+        #endregion
+
+        #region Methods
+
+        public TimeSpan ToTimeSpan()
+        {
+            return new TimeSpan(_timeSpan.Ticks);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return _timeSpan.ToString(format, formatProvider);
+        }
+
+        public string ToString(string format)
+        {
+            return _timeSpan.ToString(format);
+        }
+
+        public bool Equals(MetricTime time)
+        {
+            if (ReferenceEquals(null, time))
+                return false;
+
+            if (ReferenceEquals(this, time))
+                return true;
+
+            return TotalMicroseconds == time.TotalMicroseconds;
+        }
+
+        #endregion
+
+        #region Operators
+
+        public static MetricTime operator +(MetricTime time)
+        {
+            return time;
+        }
+
+        public static MetricTime operator +(MetricTime time1, MetricTime time2)
+        {
+            return new MetricTime(time1.TotalMicroseconds + time2.TotalMicroseconds);
+        }
+
+        public static MetricTime operator -(MetricTime time1, MetricTime time2)
+        {
+            if (time1 < time2)
+                throw new ArgumentException("First time is less than second one.", nameof(time1));
+
+            return new MetricTime(time1.TotalMicroseconds - time2.TotalMicroseconds);
+        }
+
+        public static bool operator <(MetricTime time1, MetricTime time2)
+        {
+            return time1.TotalMicroseconds < time2.TotalMicroseconds;
+        }
+
+        public static bool operator >(MetricTime time1, MetricTime time2)
+        {
+            return time1.TotalMicroseconds > time2.TotalMicroseconds;
+        }
+
+        public static bool operator <=(MetricTime time1, MetricTime time2)
+        {
+            return time1.TotalMicroseconds <= time2.TotalMicroseconds;
+        }
+
+        public static bool operator >=(MetricTime time1, MetricTime time2)
+        {
+            return time1.TotalMicroseconds >= time2.TotalMicroseconds;
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as MetricTime);
+        }
+
+        public override int GetHashCode()
+        {
+            return TotalMicroseconds.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return _timeSpan.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ DryWetMidi/Smf/Interaction/Time/MetricTimeConverter.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+
+namespace Melanchall.DryWetMidi.Smf.Interaction
+{
+    public sealed class MetricTimeConverter
+    {
+        #region Methods
+
+        public MetricTime ConvertTo(long time, TempoMap tempoMap)
+        {
+            if (time < 0)
+                throw new ArgumentOutOfRangeException(nameof(time), time, "Time is negative.");
+
+            if (tempoMap == null)
+                throw new ArgumentNullException(nameof(tempoMap));
+
+            var ticksPerQuarterNoteTimeDivision = tempoMap.TimeDivision as TicksPerQuarterNoteTimeDivision;
+            if (ticksPerQuarterNoteTimeDivision != null)
+                return ConvertToByTicksPerQuarterNote(time, ticksPerQuarterNoteTimeDivision.TicksPerQuarterNote, tempoMap);
+
+            throw new NotSupportedException("Time division other than TicksPerQuarterNoteTimeDivision not supported.");
+        }
+
+        public long ConvertFrom(MetricTime time, TempoMap tempoMap)
+        {
+            if (time == null)
+                throw new ArgumentNullException(nameof(time));
+
+            if (tempoMap == null)
+                throw new ArgumentNullException(nameof(tempoMap));
+
+            var ticksPerQuarterNoteTimeDivision = tempoMap.TimeDivision as TicksPerQuarterNoteTimeDivision;
+            if (ticksPerQuarterNoteTimeDivision != null)
+                return ConvertFromByTicksPerQuarterNote(time, ticksPerQuarterNoteTimeDivision.TicksPerQuarterNote, tempoMap);
+
+            throw new NotSupportedException("Time division other than TicksPerQuarterNoteTimeDivision not supported.");
+        }
+
+        private static MetricTime ConvertToByTicksPerQuarterNote(long time, short ticksPerQuarterNote, TempoMap tempoMap)
+        {
+            if (time < 0)
+                throw new ArgumentOutOfRangeException(nameof(time), time, "Time is negative.");
+
+            if (tempoMap == null)
+                throw new ArgumentNullException(nameof(tempoMap));
+
+            //
+
+            var tempoLine = tempoMap.TempoLine;
+            var tempoChanges = tempoLine.Values.Where(v => v.Time < time);
+            if (!tempoChanges.Any())
+                return new MetricTime(GetMicroseconds(time, Tempo.Default, ticksPerQuarterNote));
+
+            //
+
+            var accumulatedMicroseconds = 0L;
+            var lastTime = 0L;
+            var lastTempo = Tempo.Default;
+
+            foreach (var tempoChange in tempoChanges.Concat(new[] { CreateTempoChange(time, tempoLine) }))
+            {
+                var tempoChangeTime = tempoChange.Time;
+
+                accumulatedMicroseconds += GetMicroseconds(tempoChangeTime - lastTime, lastTempo, ticksPerQuarterNote);
+                lastTempo = tempoChange.Value;
+                lastTime = tempoChangeTime;
+            }
+
+            return new MetricTime(accumulatedMicroseconds);
+        }
+
+        private static long ConvertFromByTicksPerQuarterNote(MetricTime time, short ticksPerQuarterNote, TempoMap tempoMap)
+        {
+            if (time == null)
+                throw new ArgumentNullException(nameof(time));
+
+            if (tempoMap == null)
+                throw new ArgumentNullException(nameof(tempoMap));
+
+            //
+
+            var timeMicroseconds = time.TotalMicroseconds;
+
+            var accumulatedMicroseconds = 0L;
+            var lastTime = 0L;
+            var lastTempo = Tempo.Default;
+
+            foreach (var tempoChange in tempoMap.TempoLine.Values)
+            {
+                var tempoChangeTime = tempoChange.Time;
+
+                var microseconds = GetMicroseconds(tempoChangeTime - lastTime, lastTempo, ticksPerQuarterNote);
+                if (accumulatedMicroseconds + microseconds >= timeMicroseconds)
+                    break;
+
+                accumulatedMicroseconds += microseconds;
+                lastTempo = tempoChange.Value;
+                lastTime = tempoChangeTime;
+            }
+
+            return lastTime + (timeMicroseconds - accumulatedMicroseconds) / GetMicrosecondsInTick(lastTempo, ticksPerQuarterNote);
+        }
+
+        private static ValueChange<Tempo> CreateTempoChange(long time, ValuesLine<Tempo> tempoLine)
+        {
+            return new ValueChange<Tempo>(time, tempoLine.AtTime(time));
+        }
+
+        private static long GetMicroseconds(long time, Tempo tempo, short ticksPerQuarterNote)
+        {
+            return time * GetMicrosecondsInTick(tempo, ticksPerQuarterNote);
+        }
+
+        private static long GetMicrosecondsInTick(Tempo tempo, short ticksPerQuarterNote)
+        {
+            return tempo.MicrosecondsPerBeat / ticksPerQuarterNote;
+        }
+
+        #endregion
+    }
+}
