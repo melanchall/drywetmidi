be8af3045ed764b4aaf50f5696735d982e194b36
Melanchall <m.hall.muz@gmail.com>
2017-07-31 15:43:31 +0700

    Removed Tests project

---
+++ DryWetMidi.Tests/Smf/Events/BaseEventTests.cs
@@ -0,0 +1,39 @@
+﻿using Melanchall.DryWetMidi.Smf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Melanchall.DryWetMidi.Tests.Smf
+{
+    public abstract class BaseEventTests<TEvent> where TEvent : MidiEvent
+    {
+        #region Test methods
+
+        [TestMethod]
+        [Description("The Clone method produces an instance that equals the original one with delta-time of zero.")]
+        public void OriginalAndCloneAreEqual_ZeroDeltaTime()
+        {
+            var midiEvent = CreateEvent1();
+            Assert.AreEqual(midiEvent, midiEvent.Clone());
+        }
+
+        [TestMethod]
+        [Description("The Clone method produces an instance that equals the original one with nonzero delta-time.")]
+        public void OriginalAndCloneAreEqual_NonzeroDeltaTime()
+        {
+            var midiEvent = CreateEvent1();
+            midiEvent.DeltaTime = 100;
+
+            Assert.AreEqual(midiEvent, midiEvent.Clone());
+            Assert.AreEqual(midiEvent.DeltaTime, midiEvent.Clone().DeltaTime);
+        }
+
+        #endregion
+
+        #region Abstract methods
+
+        protected abstract TEvent CreateEvent1();
+
+        protected abstract TEvent CreateEvent2();
+
+        #endregion
+    }
+}
