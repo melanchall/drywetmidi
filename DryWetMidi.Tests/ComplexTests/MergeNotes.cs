36753276228ccd6ded2e0e73b50ef4e0fb0400b5
Maxim K. Dobroselsky <mdobroselsky@xtools.pro>
2017-08-09 14:01:11 +0700

    Added complex tests

---
+++ DryWetMidi.Tests/ComplexTests/MergeNotes.cs
@@ -0,0 +1,84 @@
+﻿using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Melanchall.DryWetMidi.Tests.ComplexTests
+{
+    [TestClass]
+    public class MergeNotes
+    {
+        #region Test methods
+
+        [TestMethod]
+        [Description("All notes have the same channel should be merged into one.")]
+        public void Merge_SingleChannel_MergeAll()
+        {
+            var midiFile = new MidiFile(new TrackChunk());
+
+            using (var notesManager = midiFile.GetTrackChunks().First().ManageNotes())
+            {
+                var notes = notesManager.Notes;
+                notes.Add(new Note(SevenBitNumber.MaxValue, 10),
+                          new Note(SevenBitNumber.MaxValue, 10, 2),
+                          new Note(SevenBitNumber.MaxValue, 10, 9),
+                          new Note(SevenBitNumber.MaxValue, 10, 1),
+                          new Note(SevenBitNumber.MaxValue, 10, 16),
+                          new Note(SevenBitNumber.MaxValue, 10, 25));
+            }
+
+            Merge(midiFile);
+
+            var actualNotes = midiFile.GetNotes().ToList();
+
+            Assert.AreEqual(1, actualNotes.Count);
+            Assert.AreEqual(35, actualNotes.First().Length);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static void Merge(MidiFile midiFile)
+        {
+            foreach (var trackChunk in midiFile.GetTrackChunks())
+            {
+                Merge(trackChunk);
+            }
+        }
+
+        private static void Merge(TrackChunk trackChunk)
+        {
+            using (var notesManager = trackChunk.ManageNotes())
+            {
+                var notes = notesManager.Notes;
+                var currentNotes = new Dictionary<FourBitNumber, Note>();
+
+                foreach (var note in notes.ToList())
+                {
+                    var channel = note.Channel;
+
+                    Note currentNote;
+                    if (!currentNotes.TryGetValue(channel, out currentNote))
+                    {
+                        currentNotes.Add(channel, currentNote = note);
+                        continue;
+                    }
+
+                    var currentEndTime = currentNote.Time + currentNote.Length;
+                    if (note.Time < currentEndTime)
+                    {
+                        var endTime = Math.Max(note.Time + note.Length, currentEndTime);
+                        currentNote.Length = endTime - currentNote.Time;
+
+                        notes.Remove(note);
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ DryWetMidi.Tests/ComplexTests/QuantizeNotes.cs
@@ -0,0 +1,65 @@
+﻿using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Melanchall.DryWetMidi.Tests.ComplexTests
+{
+    [TestClass]
+    public class QuantizeNotes
+    {
+        #region Test methods
+
+        [TestMethod]
+        [Description("Quantize notes by quarter-step grid.")]
+        public void Quantize_Musical_Quarter()
+        {
+            var midiFile = new MidiFile(new TrackChunk())
+            {
+                TimeDivision = new TicksPerQuarterNoteTimeDivision(10)
+            };
+
+            using (var notesManager = midiFile.GetTrackChunks().First().ManageNotes())
+            {
+                var notes = notesManager.Notes;
+                notes.Add(new Note(SevenBitNumber.MaxValue, 10, 0),
+                          new Note(SevenBitNumber.MaxValue, 10, 1),
+                          new Note(SevenBitNumber.MaxValue, 10, 8),
+                          new Note(SevenBitNumber.MaxValue, 10, 5),
+                          new Note(SevenBitNumber.MaxValue, 10, 15),
+                          new Note(SevenBitNumber.MaxValue, 10, 19));
+            }
+
+            Quantize(midiFile, MusicalFraction.Quarter);
+
+            var actualTimes = midiFile.GetNotes()
+                                      .Select(n => n.Time)
+                                      .Distinct()
+                                      .ToList();
+            var expectedTimes = Enumerable.Range(0, actualTimes.Count)
+                                          .Select(i => i * 10L)
+                                          .ToList();
+
+            CollectionAssert.AreEqual(actualTimes, expectedTimes);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static void Quantize(MidiFile midiFile, MusicalLength step)
+        {
+            var tempoMap = midiFile.GetTempoMap();
+            var stepTicks = LengthConverter.ConvertFrom(step, 0, tempoMap);
+
+            midiFile.ProcessNotes(n =>
+            {
+                var time = n.Time;
+                n.Time = (long)Math.Round(time / (double)stepTicks) * stepTicks;
+            });
+        }
+
+        #endregion
+    }
+}
