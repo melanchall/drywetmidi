97edcedbc1415e5132a0fbad3317e4158c522eef
Melanchall <m.hall.muz@gmail.com>
2017-09-17 17:52:59 +0700

    Added tests for MidiTimeSpan

---
+++ DryWetMidi.Tests/Smf.Interaction/TimeSpan/MidiTimeSpanTests.cs
@@ -0,0 +1,160 @@
+﻿using Melanchall.DryWetMidi.Smf.Interaction;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Melanchall.DryWetMidi.Tests.Smf.Interaction
+{
+    [TestClass]
+    public sealed class MidiTimeSpanTests
+    {
+        #region TestMethods
+
+        [TestMethod]
+        [Description("Try parse MIDI time span in form of 'length'.")]
+        public void TryParse_Valid()
+        {
+            TimeSpanTestingUtilities.TryParse("123", (MidiTimeSpan)123);
+        }
+
+        [TestMethod]
+        [Description("Try parse negative MIDI time span.")]
+        public void TryParse_Inalid_Negative()
+        {
+            Assert.IsFalse(MidiTimeSpan.TryParse("-234", out var length));
+        }
+
+        [TestMethod]
+        [Description("Try parse NaN time span.")]
+        public void TryParse_Inalid_NaN()
+        {
+            Assert.IsFalse(MidiTimeSpan.TryParse("abc", out var length));
+        }
+
+        [TestMethod]
+        [Description("Parse string representation of a time span.")]
+        public void Parse_ToString()
+        {
+            TimeSpanTestingUtilities.ParseToString(new MidiTimeSpan(987));
+        }
+
+        [TestMethod]
+        [Description("Add MIDI time span.")]
+        public void Add_Midi()
+        {
+            var actual = ((MidiTimeSpan)300).Add((MidiTimeSpan)40);
+            var expected = (MidiTimeSpan)340;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [Description("Add non-MIDI time to MIDI time.")]
+        public void Add_NonMidi_TimeTime()
+        {
+            TimeSpanTestingUtilities.Add_TimeTime((MidiTimeSpan)300, new MetricTimeSpan(0, 1, 30));
+        }
+
+        [TestMethod]
+        [Description("Add non-MIDI length to MIDI time.")]
+        public void Add_NonMidi_TimeLength()
+        {
+            TimeSpanTestingUtilities.Add_TimeLength((MidiTimeSpan)300, new MetricTimeSpan(0, 1, 30));
+        }
+
+        [TestMethod]
+        [Description("Add non-MIDI length to MIDI length.")]
+        public void Add_NonMidi_LengthLength()
+        {
+            TimeSpanTestingUtilities.Add_LengthLength((MidiTimeSpan)300, new MetricTimeSpan(0, 1, 30));
+        }
+
+        [TestMethod]
+        [Description("Subtract MIDI time span.")]
+        public void Subtract_Midi()
+        {
+            var actual = ((MidiTimeSpan)300).Subtract((MidiTimeSpan)40);
+            var expected = (MidiTimeSpan)260;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [Description("Subtract MIDI time span.")]
+        public void Subtract_Musical()
+        {
+            var actual = ((MidiTimeSpan)300).Subtract((MusicalTimeSpan)MusicalFraction.QuarterDotted, MathOperationMode.LengthLength);
+            var expected = new MathTimeSpan((MidiTimeSpan)300,
+                                            (MusicalTimeSpan)MusicalFraction.QuarterDotted,
+                                            MathOperation.Subtract,
+                                            MathOperationMode.LengthLength);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [Description("Calculate difference between MIDI and metric time spans.")]
+        public void Subtract_Metric_TimeTime_TempoChanged()
+        {
+            // Check that t2 + (t1 - t2) == t1 in MIDI ticks
+
+            var time1 = new MidiTimeSpan(30000);
+            var time2 = new MetricTimeSpan(0, 1, 0);
+
+            using (var tempoMapManager = new TempoMapManager())
+            {
+                tempoMapManager.SetTempo(new MetricTime(0, 0, 30), Tempo.FromBeatsPerMinute(200));
+                tempoMapManager.SetTempo(new MetricTime(0, 2, 0), Tempo.FromBeatsPerMinute(90));
+
+                var tempoMap = tempoMapManager.TempoMap;
+
+                var expected = TimeConverter2.ConvertFrom(time1, tempoMap);
+
+                var length = time1.Subtract(time2, MathOperationMode.TimeTime);
+                var actual = TimeConverter2.ConvertFrom(time2.Add(length, MathOperationMode.TimeLength), tempoMap);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod]
+        [Description("Multiply MIDI time span.")]
+        public void Multiply()
+        {
+            var actual = ((MidiTimeSpan)350).Multiply(3);
+            var expected = (MidiTimeSpan)1050;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [Description("Multiply MIDI time span by double value.")]
+        public void Multiply_Double()
+        {
+            var actual = ((MidiTimeSpan)350).Multiply(3.5);
+            var expected = (MidiTimeSpan)1225;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [Description("Divide MIDI time span.")]
+        public void Divide()
+        {
+            var actual = ((MidiTimeSpan)450).Divide(3);
+            var expected = (MidiTimeSpan)150;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [Description("Divide MIDI time span by double value.")]
+        public void Divide_Double()
+        {
+            var actual = ((MidiTimeSpan)1225).Divide(3.5);
+            var expected = (MidiTimeSpan)350;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+    }
+}
---
+++ DryWetMidi.Tests/Smf.Interaction/TimeSpan/TimeSpanTestingUtilities.cs
@@ -0,0 +1,91 @@
+﻿using Melanchall.DryWetMidi.Smf.Interaction;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Melanchall.DryWetMidi.Tests.Smf.Interaction
+{
+    internal static class TimeSpanTestingUtilities
+    {
+        #region Methods
+
+        public static void TryParse(string input, ITimeSpan expectedTimeSpan)
+        {
+            TimeSpanUtilities.TryParse(input, out var actualTimeSpan);
+            Assert.AreEqual(expectedTimeSpan, actualTimeSpan);
+        }
+
+        public static void ParseToString(ITimeSpan expectedTimeSpan)
+        {
+            Assert.AreEqual(expectedTimeSpan, TimeSpanUtilities.Parse(expectedTimeSpan.ToString()));
+        }
+
+        public static void Add_TimeTime(ITimeSpan timeSpan1, ITimeSpan timeSpan2)
+        {
+            Assert.ThrowsException<ArgumentException>(() => timeSpan1.Add(timeSpan2, MathOperationMode.TimeTime));
+        }
+
+        public static void Add_TimeLength(ITimeSpan timeSpan1, ITimeSpan timeSpan2)
+        {
+            Add(timeSpan1, timeSpan2, MathOperationMode.TimeLength);
+        }
+
+        public static void Add_LengthLength(ITimeSpan timeSpan1, ITimeSpan timeSpan2)
+        {
+            Add(timeSpan1, timeSpan2, MathOperationMode.LengthLength);
+        }
+
+        private static void Add(ITimeSpan timeSpan1, ITimeSpan timeSpan2, MathOperationMode operationMode)
+        {
+            var mathTimeSpan = timeSpan1.Add(timeSpan2, operationMode) as MathTimeSpan;
+
+            Assert.IsTrue(mathTimeSpan != null &&
+                          mathTimeSpan.TimeSpan1.Equals(timeSpan1) &&
+                          mathTimeSpan.TimeSpan2.Equals(timeSpan2) &&
+                          mathTimeSpan.Operation == MathOperation.Add &&
+                          mathTimeSpan.OperationMode == operationMode,
+                          "Result is not a math time span.");
+
+            switch (operationMode)
+            {
+                case MathOperationMode.TimeLength:
+                    Add_TimeLength_Tests(timeSpan1, timeSpan2, 100, TempoMap.Default);
+                    break;
+
+                case MathOperationMode.LengthLength:
+                    // TODO
+                    break;
+            }
+        }
+
+        private static void Add_TimeLength_Tests(ITimeSpan timeSpan1, ITimeSpan timeSpan2, long time, TempoMap tempoMap)
+        {
+            var timeSpan = timeSpan1.Add(timeSpan2, MathOperationMode.TimeLength);
+
+            //
+
+            var timeSpanMunusLength = timeSpan.Subtract(timeSpan2, MathOperationMode.TimeLength);
+
+            Assert.AreEqual(TimeConverter2.ConvertFrom(timeSpan1, tempoMap),
+                            TimeConverter2.ConvertFrom(timeSpanMunusLength, tempoMap),
+                            "(t + l) - l != t");
+
+            Assert.AreEqual(timeSpan1,
+                            TimeConverter2.ConvertTo(timeSpanMunusLength, timeSpan1.GetType(), tempoMap),
+                            "[(t + l) - l]_t != t");
+
+            //
+
+            var timeSpanMunusTime = timeSpan.Subtract(timeSpan1, MathOperationMode.TimeTime);
+
+            Assert.AreEqual(LengthConverter2.ConvertFrom(timeSpan2, time, tempoMap),
+                            LengthConverter2.ConvertFrom(timeSpanMunusTime, time, tempoMap),
+                            "(t + l) - t != l");
+
+            Assert.AreEqual(timeSpan2,
+                            LengthConverter2.ConvertTo(timeSpanMunusTime, timeSpan2.GetType(), time, tempoMap),
+                            "[(t + l) - t]_l != l");
+        }
+
+        #endregion
+    }
+}
