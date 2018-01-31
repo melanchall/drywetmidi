32cbb99f7397510e9d91e59090158576dddd6fee
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2018-01-31 09:01:03 +0300

    Added test files. Added tests for MidiFile.Read

---
+++ DryWetMidi.Tests/Smf/MidiFileTests.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using Melanchall.DryWetMidi.Smf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Melanchall.DryWetMidi.Tests.Smf
+{
+    [TestClass]
+    public sealed class MidiFileTests
+    {
+        #region Constants
+
+        private const string FilesPath = @"..\..\..\Resources\MIDI files\Invalid";
+
+        #endregion
+
+        #region Test methods
+
+        [TestMethod]
+        [Description("Read MIDI file with invalid channel event parameter value and treat that as error.")]
+        public void Read_InvalidChannelEventParameterValue()
+        {
+            const string directoryName = "Invalid Channel Event Parameter Value";
+
+            var readingSettings = new ReadingSettings
+            {
+                InvalidChannelEventParameterValuePolicy = InvalidChannelEventParameterValuePolicy.Abort
+            };
+
+            foreach (var filePath in Directory.GetFiles(GetFilesDirectory(directoryName)))
+            {
+                Assert.ThrowsException<InvalidChannelEventParameterValueException>(
+                    () => MidiFile.Read(filePath, readingSettings),
+                    $"Exception is not thrown for {filePath}.");
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static string GetFilesDirectory(string directoryName)
+        {
+            return Path.Combine(FilesPath, directoryName);
+        }
+
+        #endregion
+    }
+}
