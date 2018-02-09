a835047b36b7239eb85a3de3ffc7f53e5837fd08
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2018-02-09 21:23:11 +0300

    Added Chord to MusicTheory namespace

---
+++ DryWetMidi.Tests/MusicTheory/Chord/ChordUtilitiesTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Melanchall.DryWetMidi.MusicTheory;
+using NUnit.Framework;
+
+namespace Melanchall.DryWetMidi.Tests.MusicTheory
+{
+    [TestFixture]
+    public sealed class ChordUtilitiesTests
+    {
+        #region Test methods
+
+        [Test]
+        [Description("Get inversion of the empty chord.")]
+        public void GetInversion_EmptyChord()
+        {
+            var chord = GetEmptyChord();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => chord.GetInversion(2));
+        }
+
+        [Test]
+        [Description("Get inversion of unallowable number (below lower limit).")]
+        public void GetInversion_OutOfRange_Below()
+        {
+            var chord = GetChord_CMajorTriad();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => chord.GetInversion(0));
+        }
+
+        [Test]
+        [Description("Get inversion of unallowable number (above upper limit).")]
+        public void GetInversion_OutOfRange_Above()
+        {
+            var chord = GetChord_CMajorTriad();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => chord.GetInversion(3));
+        }
+
+        [Test]
+        [Description("Get first inversion.")]
+        public void GetInversion_First()
+        {
+            var chord = GetChord_CMajorTriad();
+
+            Assert.AreEqual(new Chord(NoteName.E, NoteName.G, NoteName.C), chord.GetInversion(1));
+        }
+
+        [Test]
+        [Description("Get second inversion.")]
+        public void GetInversion_Second()
+        {
+            var chord = GetChord_CMajorTriad();
+
+            Assert.AreEqual(new Chord(NoteName.G, NoteName.C, NoteName.E), chord.GetInversion(2));
+        }
+
+        [Test]
+        [Description("Get inversions.")]
+        public void GetInversions()
+        {
+            var chord = GetChord_CMajorTriad();
+
+            CollectionAssert.AreEqual(new[]
+                {
+                    new Chord(NoteName.E, NoteName.G, NoteName.C),
+                    new Chord(NoteName.G, NoteName.C, NoteName.E),
+                },
+                chord.GetInversions());
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static Chord GetEmptyChord()
+        {
+            return new Chord();
+        }
+
+        private static Chord GetChord_CMajorTriad()
+        {
+            return new Chord(NoteName.C,
+                             NoteName.E,
+                             NoteName.G);
+        }
+
+        #endregion
+    }
+}
---
+++ DryWetMidi/MusicTheory/Chord/ChordUtilities.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Melanchall.DryWetMidi.Common;
+
+namespace Melanchall.DryWetMidi.MusicTheory
+{
+    public static class ChordUtilities
+    {
+        #region Methods
+
+        // TODO: optimize
+        public static Chord GetInversion(this Chord chord, int inversionNumber)
+        {
+            ThrowIfArgument.IsNull(nameof(chord), chord);
+
+            var notes = chord.Notes;
+            var inversionsCount = notes.Count() - 1;
+
+            ThrowIfArgument.IsOutOfRange(nameof(inversionNumber),
+                                         inversionNumber,
+                                         1,
+                                         inversionsCount,
+                                         "Inversion number is zero or negative.");
+
+            var head = notes.Take(inversionNumber);
+            var tail = notes.Skip(inversionNumber);
+
+            return new Chord(tail.Concat(head).ToArray());
+        }
+
+        // TODO: optimize
+        public static IEnumerable<Chord> GetInversions(this Chord chord)
+        {
+            ThrowIfArgument.IsNull(nameof(chord), chord);
+
+            var inversionsCount = chord.Notes.Count() - 1;
+            return Enumerable.Range(1, inversionsCount)
+                             .Select(i => chord.GetInversion(i));
+        }
+
+        #endregion
+    }
+}
