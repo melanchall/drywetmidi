d2b7fe3b87c8f9c81fde091aec5383ab85d43a75
melanchall <m.hall.muz@gmail.com>
2018-04-16 17:26:18 +0300

    Added splitting utilities for TrackChunk and MidiFile

---
+++ DryWetMidi/Tools/LengthedObjectsSplitter/ChordsSplitterUtilities.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+
+namespace Melanchall.DryWetMidi.Tools.aa
+{
+    public static class ChordsSplitterUtilities
+    {
+        #region Methods
+
+        public static void SplitChordsByStep(this TrackChunk trackChunk, ITimeSpan step, TempoMap tempoMap, long notesTolerance = 0)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunk), trackChunk);
+            ThrowIfArgument.IsNull(nameof(step), step);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            SplitTrackChunkChords(trackChunk, (splitter, notes) => splitter.SplitByStep(notes, step, tempoMap), notesTolerance);
+        }
+
+        public static void SplitChordsByStep(this IEnumerable<TrackChunk> trackChunks, ITimeSpan step, TempoMap tempoMap, long notesTolerance = 0)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunks), trackChunks);
+            ThrowIfArgument.IsNull(nameof(step), step);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            foreach (var trackChunk in trackChunks)
+            {
+                trackChunk.SplitChordsByStep(step, tempoMap, notesTolerance);
+            }
+        }
+
+        public static void SplitChordsByStep(this MidiFile midiFile, ITimeSpan step, long notesTolerance = 0)
+        {
+            ThrowIfArgument.IsNull(nameof(midiFile), midiFile);
+            ThrowIfArgument.IsNull(nameof(step), step);
+
+            var tempoMap = midiFile.GetTempoMap();
+
+            midiFile.GetTrackChunks().SplitChordsByStep(step, tempoMap, notesTolerance);
+        }
+
+        public static void SplitChordsByPartsNumber(this TrackChunk trackChunk, int partsNumber, TimeSpanType lengthType, TempoMap tempoMap, long notesTolerance = 0)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunk), trackChunk);
+            ThrowIfArgument.IsNonpositive(nameof(partsNumber), partsNumber, "Parts number is zero or negative.");
+            ThrowIfArgument.IsInvalidEnumValue(nameof(lengthType), lengthType);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            SplitTrackChunkChords(trackChunk, (splitter, notes) => splitter.SplitByPartsNumber(notes, partsNumber, lengthType, tempoMap), notesTolerance);
+        }
+
+        public static void SplitChordsByPartsNumber(this IEnumerable<TrackChunk> trackChunks, int partsNumber, TimeSpanType lengthType, TempoMap tempoMap, long notesTolerance = 0)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunks), trackChunks);
+            ThrowIfArgument.IsNonpositive(nameof(partsNumber), partsNumber, "Parts number is zero or negative.");
+            ThrowIfArgument.IsInvalidEnumValue(nameof(lengthType), lengthType);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            foreach (var trackChunk in trackChunks)
+            {
+                trackChunk.SplitChordsByPartsNumber(partsNumber, lengthType, tempoMap, notesTolerance);
+            }
+        }
+
+        public static void SplitChordsByPartsNumber(this MidiFile midiFile, int partsNumber, TimeSpanType lengthType, long notesTolerance = 0)
+        {
+            ThrowIfArgument.IsNull(nameof(midiFile), midiFile);
+            ThrowIfArgument.IsNonpositive(nameof(partsNumber), partsNumber, "Parts number is zero or negative.");
+            ThrowIfArgument.IsInvalidEnumValue(nameof(lengthType), lengthType);
+
+            var tempoMap = midiFile.GetTempoMap();
+
+            midiFile.GetTrackChunks().SplitChordsByPartsNumber(partsNumber, lengthType, tempoMap, notesTolerance);
+        }
+
+        public static void SplitChordsByGrid(this TrackChunk trackChunk, IGrid grid, TempoMap tempoMap, long notesTolerance = 0)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunk), trackChunk);
+            ThrowIfArgument.IsNull(nameof(grid), grid);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            SplitTrackChunkChords(trackChunk, (splitter, notes) => splitter.SplitByGrid(notes, grid, tempoMap), notesTolerance);
+        }
+
+        public static void SplitChordsByGrid(this IEnumerable<TrackChunk> trackChunks, IGrid grid, TempoMap tempoMap, long notesTolerance = 0)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunks), trackChunks);
+            ThrowIfArgument.IsNull(nameof(grid), grid);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            foreach (var trackChunk in trackChunks)
+            {
+                trackChunk.SplitChordsByGrid(grid, tempoMap, notesTolerance);
+            }
+        }
+
+        public static void SplitChordsByGrid(this MidiFile midiFile, IGrid grid, long notesTolerance = 0)
+        {
+            ThrowIfArgument.IsNull(nameof(midiFile), midiFile);
+            ThrowIfArgument.IsNull(nameof(grid), grid);
+
+            var tempoMap = midiFile.GetTempoMap();
+
+            midiFile.GetTrackChunks().SplitChordsByGrid(grid, tempoMap, notesTolerance);
+        }
+
+        private static void SplitTrackChunkChords(TrackChunk trackChunk, Func<ChordsSplitter, IEnumerable<Chord>, IEnumerable<Chord>> splitOperation, long notesTolerance)
+        {
+            using (var chordsManager = trackChunk.ManageChords(notesTolerance))
+            {
+                var chords = chordsManager.Chords;
+
+                var chordsSplitter = new ChordsSplitter();
+                var newChords = splitOperation(chordsSplitter, chords);
+
+                chords.Clear();
+                chords.Add(newChords);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ DryWetMidi/Tools/LengthedObjectsSplitter/NotesSplitterUtilities.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+
+namespace Melanchall.DryWetMidi.Tools
+{
+    public static class NotesSplitterUtilities
+    {
+        #region Methods
+
+        public static void SplitNotesByStep(this TrackChunk trackChunk, ITimeSpan step, TempoMap tempoMap)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunk), trackChunk);
+            ThrowIfArgument.IsNull(nameof(step), step);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            SplitTrackChunkNotes(trackChunk, (splitter, notes) => splitter.SplitByStep(notes, step, tempoMap));
+        }
+
+        public static void SplitNotesByStep(this IEnumerable<TrackChunk> trackChunks, ITimeSpan step, TempoMap tempoMap)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunks), trackChunks);
+            ThrowIfArgument.IsNull(nameof(step), step);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            foreach (var trackChunk in trackChunks)
+            {
+                trackChunk.SplitNotesByStep(step, tempoMap);
+            }
+        }
+
+        public static void SplitNotesByStep(this MidiFile midiFile, ITimeSpan step)
+        {
+            ThrowIfArgument.IsNull(nameof(midiFile), midiFile);
+            ThrowIfArgument.IsNull(nameof(step), step);
+
+            var tempoMap = midiFile.GetTempoMap();
+
+            midiFile.GetTrackChunks().SplitNotesByStep(step, tempoMap);
+        }
+
+        public static void SplitNotesByPartsNumber(this TrackChunk trackChunk, int partsNumber, TimeSpanType lengthType, TempoMap tempoMap)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunk), trackChunk);
+            ThrowIfArgument.IsNonpositive(nameof(partsNumber), partsNumber, "Parts number is zero or negative.");
+            ThrowIfArgument.IsInvalidEnumValue(nameof(lengthType), lengthType);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            SplitTrackChunkNotes(trackChunk, (splitter, notes) => splitter.SplitByPartsNumber(notes, partsNumber, lengthType, tempoMap));
+        }
+
+        public static void SplitNotesByPartsNumber(this IEnumerable<TrackChunk> trackChunks, int partsNumber, TimeSpanType lengthType, TempoMap tempoMap)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunks), trackChunks);
+            ThrowIfArgument.IsNonpositive(nameof(partsNumber), partsNumber, "Parts number is zero or negative.");
+            ThrowIfArgument.IsInvalidEnumValue(nameof(lengthType), lengthType);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            foreach (var trackChunk in trackChunks)
+            {
+                trackChunk.SplitNotesByPartsNumber(partsNumber, lengthType, tempoMap);
+            }
+        }
+
+        public static void SplitNotesByPartsNumber(this MidiFile midiFile, int partsNumber, TimeSpanType lengthType)
+        {
+            ThrowIfArgument.IsNull(nameof(midiFile), midiFile);
+            ThrowIfArgument.IsNonpositive(nameof(partsNumber), partsNumber, "Parts number is zero or negative.");
+            ThrowIfArgument.IsInvalidEnumValue(nameof(lengthType), lengthType);
+
+            var tempoMap = midiFile.GetTempoMap();
+
+            midiFile.GetTrackChunks().SplitNotesByPartsNumber(partsNumber, lengthType, tempoMap);
+        }
+
+        public static void SplitNotesByGrid(this TrackChunk trackChunk, IGrid grid, TempoMap tempoMap)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunk), trackChunk);
+            ThrowIfArgument.IsNull(nameof(grid), grid);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            SplitTrackChunkNotes(trackChunk, (splitter, notes) => splitter.SplitByGrid(notes, grid, tempoMap));
+        }
+
+        public static void SplitNotesByGrid(this IEnumerable<TrackChunk> trackChunks, IGrid grid, TempoMap tempoMap)
+        {
+            ThrowIfArgument.IsNull(nameof(trackChunks), trackChunks);
+            ThrowIfArgument.IsNull(nameof(grid), grid);
+            ThrowIfArgument.IsNull(nameof(tempoMap), tempoMap);
+
+            foreach (var trackChunk in trackChunks)
+            {
+                trackChunk.SplitNotesByGrid(grid, tempoMap);
+            }
+        }
+
+        public static void SplitNotesByGrid(this MidiFile midiFile, IGrid grid)
+        {
+            ThrowIfArgument.IsNull(nameof(midiFile), midiFile);
+            ThrowIfArgument.IsNull(nameof(grid), grid);
+
+            var tempoMap = midiFile.GetTempoMap();
+
+            midiFile.GetTrackChunks().SplitNotesByGrid(grid, tempoMap);
+        }
+
+        private static void SplitTrackChunkNotes(TrackChunk trackChunk, Func<NotesSplitter, IEnumerable<Note>, IEnumerable<Note>> splitOperation)
+        {
+            using (var notesManager = trackChunk.ManageNotes())
+            {
+                var notes = notesManager.Notes;
+
+                var notesSplitter = new NotesSplitter();
+                var newNotes = splitOperation(notesSplitter, notes);
+
+                notes.Clear();
+                notes.Add(newNotes);
+            }
+        }
+
+        #endregion
+    }
+}
