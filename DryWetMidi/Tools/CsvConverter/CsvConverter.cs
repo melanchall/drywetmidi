fd7069c6fa3cd46e2219b246e15f985ff047bb94
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2018-06-27 22:33:46 +0300

    Added CsvConverter: MIDI file to CSV

---
+++ DryWetMidi/Tools/CsvConverter/CsvConverter.cs
@@ -0,0 +1,24 @@
+﻿using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Smf;
+
+namespace Melanchall.DryWetMidi.Tools
+{
+    public sealed class CsvConverter
+    {
+        #region Methods
+
+        public void ConvertMidiFileToCsv(MidiFile midiFile, string outputFilePath, bool overwriteFile = false, MidiFileCsvConversionSettings settings = null)
+        {
+            ThrowIfArgument.IsNull(nameof(midiFile), midiFile);
+
+            using (var fileStream = FileUtilities.OpenFileForWrite(outputFilePath, overwriteFile))
+            {
+                MidiFileToCsvConverter.ConvertToCsv(midiFile,
+                                                    fileStream,
+                                                    settings ?? new MidiFileCsvConversionSettings());
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ DryWetMidi/Tools/CsvConverter/MidiFileCsv/MidiFileCsvConversionSettings.cs
@@ -0,0 +1,41 @@
+﻿using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Smf.Interaction;
+
+namespace Melanchall.DryWetMidi.Tools
+{
+    public sealed class MidiFileCsvConversionSettings
+    {
+        #region Fields
+
+        private MidiFileCsvLayout _csvLayout = MidiFileCsvLayout.DryWetMidi;
+        private TimeSpanType _timeType = TimeSpanType.Midi;
+
+        #endregion
+
+        #region Properties
+
+        public MidiFileCsvLayout CsvLayout
+        {
+            get { return _csvLayout; }
+            set
+            {
+                ThrowIfArgument.IsInvalidEnumValue(nameof(value), value);
+
+                _csvLayout = value;
+            }
+        }
+
+        public TimeSpanType TimeType
+        {
+            get { return _timeType; }
+            set
+            {
+                ThrowIfArgument.IsInvalidEnumValue(nameof(value), value);
+
+                _timeType = value;
+            }
+        }
+
+        #endregion
+    }
+}
