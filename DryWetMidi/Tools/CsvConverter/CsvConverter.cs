c182e501cf892a15e052911fa1cb8ac20c7a6219
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2018-07-03 22:09:50 +0300

    Added conversion MidiFile to/from CSV via streams

--- DryWetMidi/Tools/CsvConverter/CsvConverter.cs
+++ DryWetMidi/Tools/CsvConverter/CsvConverter.cs
@@ -1,4 +1,6 @@
-﻿using Melanchall.DryWetMidi.Common;
+﻿using System;
+using System.IO;
+using Melanchall.DryWetMidi.Common;
 using Melanchall.DryWetMidi.Smf;
 
 namespace Melanchall.DryWetMidi.Tools
@@ -13,19 +15,37 @@ namespace Melanchall.DryWetMidi.Tools
 
             using (var fileStream = FileUtilities.OpenFileForWrite(outputFilePath, overwriteFile))
             {
-                MidiFileToCsvConverter.ConvertToCsv(midiFile,
-                                                    fileStream,
-                                                    settings ?? new MidiFileCsvConversionSettings());
+                ConvertMidiFileToCsv(midiFile, fileStream, settings);
             }
+        }
+
+        public void ConvertMidiFileToCsv(MidiFile midiFile, Stream stream, MidiFileCsvConversionSettings settings = null)
+        {
+            ThrowIfArgument.IsNull(nameof(midiFile), midiFile);
+            ThrowIfArgument.IsNull(nameof(stream), stream);
+
+            if (!stream.CanWrite)
+                throw new ArgumentException("Stream doesn't support writing.", nameof(stream));
+
+            MidiFileToCsvConverter.ConvertToCsv(midiFile, stream, settings ?? new MidiFileCsvConversionSettings());
         }
 
         public MidiFile ConvertCsvToMidiFile(string filePath, MidiFileCsvConversionSettings settings = null)
         {
             using (var fileStream = FileUtilities.OpenFileForRead(filePath))
             {
-                return CsvToMidiFileConverter.ConvertToMidiFile(fileStream,
-                                                                settings ?? new MidiFileCsvConversionSettings());
+                return ConvertCsvToMidiFile(fileStream, settings);
             }
+        }
+
+        public MidiFile ConvertCsvToMidiFile(Stream stream, MidiFileCsvConversionSettings settings = null)
+        {
+            ThrowIfArgument.IsNull(nameof(stream), stream);
+
+            if (!stream.CanRead)
+                throw new ArgumentException("Stream doesn't support reading.", nameof(stream));
+
+            return CsvToMidiFileConverter.ConvertToMidiFile(stream, settings ?? new MidiFileCsvConversionSettings());
         }
 
         #endregion
