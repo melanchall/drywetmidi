b89ba3ffd00e51b517060d5c0d0ef11bfae0e7f6
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2018-07-28 15:26:08 +0300

    Added ShiftEvents method

---
+++ DryWetMidi.Tests/Smf.Interaction/Utilities/MidiFileUtilitiesTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+using Melanchall.DryWetMidi.Tests.Utilities;
+using NUnit.Framework;
+
+namespace Melanchall.DryWetMidi.Tests.Smf.Interaction
+{
+    [TestFixture]
+    public sealed class MidiFileUtilitiesTests
+    {
+        #region Test methods
+
+        [Test]
+        public void ShiftEvents_ValidFiles_Midi()
+        {
+            var distance = 10000;
+
+            foreach (var filePath in TestFilesProvider.GetValidFiles())
+            {
+                var midiFile = MidiFile.Read(filePath);
+                var originalTimes = midiFile.GetTimedEvents().Select(e => e.Time).ToList();
+
+                midiFile.ShiftEvents((MidiTimeSpan)distance);
+                var newTimes = midiFile.GetTimedEvents().Select(e => e.Time).ToList();
+
+                Assert.IsTrue(midiFile.GetTimedEvents().All(e => e.Time >= distance), "Some events are not shifted.");
+                CollectionAssert.AreEqual(originalTimes, newTimes.Select(t => t - distance));
+            }
+        }
+
+        [Test]
+        public void ShiftEvents_ValidFiles_Metric()
+        {
+            var distance = new MetricTimeSpan(0, 1, 0);
+
+            foreach (var filePath in TestFilesProvider.GetValidFiles())
+            {
+                var midiFile = MidiFile.Read(filePath);
+                midiFile.ShiftEvents(distance);
+
+                var tempoMap = midiFile.GetTempoMap();
+
+                Assert.IsTrue(midiFile.GetTimedEvents()
+                                      .Select(e => e.TimeAs<MetricTimeSpan>(tempoMap).CompareTo(distance))
+                                      .All(t => t >= 0),
+                              "Some events are not shifted.");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ DryWetMidi/Smf.Interaction/Utilities/MidiFileUtilities.cs
@@ -0,0 +1,19 @@
+﻿using Melanchall.DryWetMidi.Common;
+
+namespace Melanchall.DryWetMidi.Smf.Interaction
+{
+    public static class MidiFileUtilities
+    {
+        #region Methods
+
+        public static void ShiftEvents(this MidiFile midiFile, ITimeSpan distance)
+        {
+            ThrowIfArgument.IsNull(nameof(midiFile), midiFile);
+            ThrowIfArgument.IsNull(nameof(distance), distance);
+
+            midiFile.GetTrackChunks().ShiftEvents(distance, midiFile.TimeDivision);
+        }
+
+        #endregion
+    }
+}
