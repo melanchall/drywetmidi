3eba945b35699053a31d51c7b273a1b25b188226
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2018-09-16 16:48:30 +0300

    Prepare version

--- DryWetMidi/Properties/AssemblyInfo.cs
+++ DryWetMidi/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DryWetMIDI")]
-[assembly: AssemblyDescription(".NET library to work with MIDI files")]
+[assembly: AssemblyDescription(".NET library to work with MIDI files and MIDI devices")]
 [assembly: AssemblyProduct("DryWetMIDI")]
 [assembly: AssemblyCopyright("Copyright © Melanchall 2018")]
 
