0483df790f960a9d97d224515275080e5753a3c3
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-01-18 10:12:43 +0300

    Added more playback tests

---
+++ DryWetMidi.Tests/Devices/SendReceiveTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+using Melanchall.DryWetMidi.Tests.Utilities;
+using NUnit.Framework;
+
+namespace Melanchall.DryWetMidi.Tests.Devices
+{
+    [TestFixture]
+    public sealed class SendReceiveTests
+    {
+        #region Test methods
+
+        [Test]
+        public void CheckEventsReceiving()
+        {
+            SendReceiveUtilities.CheckEventsReceiving(new[]
+            {
+                new EventToSend(new NoteOnEvent((SevenBitNumber)100, (SevenBitNumber)20) { Channel = (FourBitNumber)5 }, TimeSpan.Zero),
+                new EventToSend(new NormalSysExEvent(new byte[] { 1, 2, 3, 0xF7 }), TimeSpan.FromSeconds(1)),
+                new EventToSend(new NoteOffEvent((SevenBitNumber)100, (SevenBitNumber)10) { Channel = (FourBitNumber)5 }, TimeSpan.FromSeconds(2)),
+                new EventToSend(new NormalSysExEvent(new byte[] { 4, 5, 6, 0xF7 }), TimeSpan.FromSeconds(2)),
+                new EventToSend(new SongSelectEvent((SevenBitNumber)20), TimeSpan.Zero),
+                new EventToSend(new TuneRequestEvent(), TimeSpan.FromMilliseconds(200)),
+            });
+        }
+
+        [Test]
+        public void CheckFileEventsReceiving()
+        {
+            var filesToTest = TestFilesProvider.GetValidFiles(
+                f => f.GetTrackChunks().Count() == 1,
+                f =>
+                {
+                    var tempoMap = f.GetTempoMap();
+                    return (TimeSpan)f.GetTimedEvents().Last().TimeAs<MetricTimeSpan>(tempoMap) < TimeSpan.FromSeconds(30);
+                })
+                .Take(5)
+                .ToArray();
+
+            for (var i = 0; i < filesToTest.Length; i++)
+            {
+                var file = filesToTest[i];
+                var tempoMap = file.GetTempoMap();
+
+                var eventsToSend = new List<EventToSend>();
+                var currentTime = TimeSpan.Zero;
+
+                foreach (var timedEvent in file.GetTimedEvents().Where(e => !(e.Event is MetaEvent)))
+                {
+                    var time = (TimeSpan)timedEvent.TimeAs<MetricTimeSpan>(tempoMap);
+                    var eventToSend = new EventToSend(timedEvent.Event, time - currentTime);
+                    currentTime = time;
+                    eventsToSend.Add(eventToSend);
+                }
+
+                SendReceiveUtilities.CheckEventsReceiving(eventsToSend);
+            }
+        }
+
+        #endregion
+    }
+}
