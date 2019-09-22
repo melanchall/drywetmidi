dbfd8d4a97ada098601ee76ffde44daeaa55b324
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-09-22 19:08:26 +0300

    Added Pattern.SplitAtAnchor

--- DryWetMidi.Tests/Composing/PatternTests.Misc.cs
+++ DryWetMidi.Tests/Composing/PatternTests.Misc.cs
@@ -216,6 +216,114 @@ namespace Melanchall.DryWetMidi.Tests.Composing
             });
         }
 
+        [Test]
+        public void SplitAtAnchor_Empty()
+        {
+            var pattern = new PatternBuilder().Build();
+            var patterns = pattern.SplitAtAnchor(new object());
+            CollectionAssert.IsEmpty(patterns, "Pattern splitted incorrectly.");
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void SplitAtAnchor_NoActionsBetweenAnchors(bool removeEmptyPatterns)
+        {
+            var anchor = "A";
+            var pattern = new PatternBuilder()
+                .Anchor(anchor)
+                .Anchor(anchor)
+                .Build();
+
+            var patterns = pattern.SplitAtAnchor(anchor, removeEmptyPatterns).ToList();
+
+            if (removeEmptyPatterns)
+                CollectionAssert.IsEmpty(patterns, "Pattern splitted incorrectly.");
+            else
+            {
+                Assert.AreEqual(2, patterns.Count, "Sub-patterns count is invalid.");
+                CollectionAssert.IsEmpty(patterns[0].Actions, "First sub-pattern is not empty.");
+                CollectionAssert.IsEmpty(patterns[1].Actions, "Second sub-pattern is not empty.");
+            }
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void SplitAtAnchor_SingleAnchor(bool removeEmptyPatterns)
+        {
+            var anchor = "A";
+            var pattern = new PatternBuilder()
+                .Anchor(anchor)
+                .Note(Notes.FSharp4)
+                .Note(Notes.DSharp1)
+                .Anchor(anchor)
+                .Note(Notes.DSharp0)
+                .Anchor(anchor)
+                .Build();
+
+            var patterns = pattern.SplitAtAnchor(anchor, removeEmptyPatterns).ToList();
+
+            var firstPatternIndex = 0;
+            var secondPatternIndex = 1;
+
+            if (removeEmptyPatterns)
+                Assert.AreEqual(2, patterns.Count, "Sub-patterns count is invalid.");
+            else
+            {
+                Assert.AreEqual(3, patterns.Count, "Sub-patterns count is invalid.");
+                CollectionAssert.IsEmpty(patterns[0].Actions, "First sub-pattern is not empty.");
+                firstPatternIndex++;
+                secondPatternIndex++;
+            }
+
+            TestNotes(patterns[firstPatternIndex], new[]
+            {
+                new NoteInfo(NoteName.FSharp, 4, null, PatternBuilder.DefaultNoteLength),
+                new NoteInfo(NoteName.DSharp, 1, PatternBuilder.DefaultNoteLength, PatternBuilder.DefaultNoteLength)
+            });
+
+            TestNotes(patterns[secondPatternIndex], new[]
+            {
+                new NoteInfo(NoteName.DSharp, 0, null, PatternBuilder.DefaultNoteLength)
+            });
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void SplitAtAnchor_MultipleAnchors(bool removeEmptyPatterns)
+        {
+            var anchor1 = "A";
+            var anchor2 = "B";
+
+            var pattern = new PatternBuilder()
+                .Anchor(anchor1)
+                .Note(Notes.FSharp4)
+                .Note(Notes.DSharp1)
+                .Anchor(anchor2)
+                .Note(Notes.DSharp0)
+                .Anchor(anchor1)
+                .Build();
+
+            var patterns = pattern.SplitAtAnchor(anchor1, removeEmptyPatterns).ToList();
+
+            var subPatternIndex = 0;
+
+            if (removeEmptyPatterns)
+                Assert.AreEqual(1, patterns.Count, "Sub-patterns count is invalid.");
+            else
+            {
+                Assert.AreEqual(2, patterns.Count, "Sub-patterns count is invalid.");
+                CollectionAssert.IsEmpty(patterns[0].Actions, "First sub-pattern is not empty.");
+                subPatternIndex++;
+            }
+
+            TestNotes(patterns[subPatternIndex], new[]
+            {
+                new NoteInfo(NoteName.FSharp, 4, null, PatternBuilder.DefaultNoteLength),
+                new NoteInfo(NoteName.DSharp, 1, PatternBuilder.DefaultNoteLength, PatternBuilder.DefaultNoteLength),
+                new NoteInfo(NoteName.DSharp, 0, PatternBuilder.DefaultNoteLength.Multiply(2), PatternBuilder.DefaultNoteLength)
+            });
+        }
+
         #endregion
     }
 }
