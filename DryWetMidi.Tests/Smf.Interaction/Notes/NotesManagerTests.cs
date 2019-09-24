e2c0ed6e151a4bd54e40a1e8087e84a833c5d2d2
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-09-24 15:35:52 +0300

    Fixed notes order is invalid on collecting by NotesManager when notes are overlapped

--- DryWetMidi.Tests/Smf.Interaction/Notes/NotesManagerTests.cs
+++ DryWetMidi.Tests/Smf.Interaction/Notes/NotesManagerTests.cs
@@ -1,5 +1,7 @@
-﻿using Melanchall.DryWetMidi.Smf;
+﻿using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Smf;
 using Melanchall.DryWetMidi.Smf.Interaction;
+using Melanchall.DryWetMidi.Tests.Utilities;
 using NUnit.Framework;
 
 namespace Melanchall.DryWetMidi.Tests.Smf.Interaction
@@ -23,6 +25,31 @@ namespace Melanchall.DryWetMidi.Tests.Smf.Interaction
                 notes.Add(NoteTestUtilities.GetNoteByTime(45));
 
                 TimedObjectsCollectionTestUtilities.CheckTimedObjectsCollectionTimes(notes, 1, 10, 45, 123);
+            }
+        }
+
+        [Test]
+        public void Notes_Overlapped()
+        {
+            var trackChunk = new TrackChunk(
+                new NoteOnEvent((SevenBitNumber)30, (SevenBitNumber)70),
+                new NoteOnEvent((SevenBitNumber)60, (SevenBitNumber)50),
+                new NoteOffEvent((SevenBitNumber)60, (SevenBitNumber)0) { DeltaTime = 100 },
+                new NoteOnEvent((SevenBitNumber)40, (SevenBitNumber)70),
+                new NoteOffEvent((SevenBitNumber)40, (SevenBitNumber)0) { DeltaTime = 100 },
+                new NoteOffEvent((SevenBitNumber)30, (SevenBitNumber)0));
+
+            var expectedNotes = new[]
+            {
+                new Note((SevenBitNumber)30, 200, 0) { Velocity = (SevenBitNumber)70 },
+                new Note((SevenBitNumber)60, 100, 0) { Velocity = (SevenBitNumber)50 },
+                new Note((SevenBitNumber)40, 100, 100) { Velocity = (SevenBitNumber)70 }
+            };
+
+            using (var notesManager = trackChunk.ManageNotes())
+            {
+                var notes = notesManager.Notes;
+                Assert.IsTrue(TimedObjectEquality.AreEqual(expectedNotes, notes, true));
             }
         }
 
