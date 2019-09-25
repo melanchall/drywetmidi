ff7d445efbf1f42f97726a0ca6822ae90fca42ae
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-09-25 21:29:38 +0300

    Added Chord class to MusicTheory namespace

---
+++ DryWetMidi.Tests/MusicTheory/Chord/ChordTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using Melanchall.DryWetMidi.MusicTheory;
+using NUnit.Framework;
+
+namespace Melanchall.DryWetMidi.Tests.MusicTheory
+{
+    [TestFixture]
+    public sealed class ChordTests
+    {
+        #region Test methods
+
+        [Test]
+        public void CreateWithEmptyNotesCollection()
+        {
+            Assert.Throws<ArgumentException>(() => new Chord(Enumerable.Empty<NoteName>()));
+        }
+
+        [Test]
+        public void CreateWithInvalidNotes()
+        {
+            Assert.Throws<InvalidEnumArgumentException>(() => new Chord((NoteName)100));
+        }
+
+        [Test]
+        public void CreateWithValidNotes()
+        {
+            var chord = new Chord(NoteName.A, NoteName.B);
+            CollectionAssert.AreEqual(new[] { NoteName.A, NoteName.B }, chord.Notes, "Notes are invalid.");
+        }
+
+        [Test]
+        public void CheckEquality()
+        {
+            var chord1 = new Chord(NoteName.A, NoteName.B);
+            var chord2 = new Chord(NoteName.A, NoteName.B);
+            var chord3 = new Chord(NoteName.B, NoteName.A);
+
+            Assert.AreEqual(chord1, chord2, "Chords are not equal.");
+            Assert.AreNotEqual(chord1, chord3, "Chords are equal.");
+        }
+
+        #endregion
+    }
+}
---
+++ DryWetMidi/MusicTheory/Chord/Chord.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Melanchall.DryWetMidi.Common;
+
+namespace Melanchall.DryWetMidi.MusicTheory
+{
+    public sealed class Chord
+    {
+        #region Constructor
+
+        public Chord(IEnumerable<NoteName> notes)
+        {
+            ThrowIfArgument.IsNull(nameof(notes), notes);
+            ThrowIfArgument.ContainsInvalidEnumValue(nameof(notes), notes);
+            ThrowIfArgument.IsEmptyCollection(nameof(notes), notes, "Notes collection is empty.");
+
+            Notes = notes;
+        }
+
+        public Chord(params NoteName[] notes)
+            : this(notes as IEnumerable<NoteName>)
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public IEnumerable<NoteName> Notes { get; }
+
+        #endregion
+
+        #region Operators
+
+        public static bool operator ==(Chord chord1, Chord chord2)
+        {
+            if (ReferenceEquals(chord1, chord2))
+                return true;
+
+            if (ReferenceEquals(null, chord1) || ReferenceEquals(null, chord2))
+                return false;
+
+            return chord1.Notes.SequenceEqual(chord2.Notes);
+        }
+
+        public static bool operator !=(Chord chord1, Chord chord2)
+        {
+            return !(chord1 == chord2);
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override string ToString()
+        {
+            return string.Join(" ", Notes.Select(n => n.ToString().Replace(Note.SharpLongString, Note.SharpShortString)));
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this == (obj as Chord);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var result = 17;
+
+                foreach (var note in Notes)
+                {
+                    result = result * 23 + note.GetHashCode();
+                }
+
+                return result;
+            }
+        }
+
+        #endregion
+    }
+}
