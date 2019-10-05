8046f0df3c46c129e046005a4eac59d8319f88ba
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-10-05 17:40:01 +0300

    Added NoteUtilities.Transpose method for NoteName

---
+++ DryWetMidi.Tests/MusicTheory/Note/NoteUtilitiesTests.cs
@@ -0,0 +1,37 @@
+﻿using Melanchall.DryWetMidi.MusicTheory;
+using NUnit.Framework;
+
+namespace Melanchall.DryWetMidi.Tests.MusicTheory
+{
+    [TestFixture]
+    public sealed class NoteUtilitiesTests
+    {
+        #region Constants
+
+        private static readonly object[] ParametersTransposeCheck =
+        {
+            new object[] { NoteName.A, 0, NoteName.A },
+            new object[] { NoteName.A, 3, NoteName.C },
+            new object[] { NoteName.A, 12, NoteName.A },
+            new object[] { NoteName.A, 36, NoteName.A },
+            new object[] { NoteName.A, -3, NoteName.FSharp },
+            new object[] { NoteName.A, -12, NoteName.A },
+            new object[] { NoteName.A, -36, NoteName.A },
+        };
+
+        #endregion
+
+        #region Test methods
+
+        [Test]
+        [TestCaseSource(nameof(ParametersTransposeCheck))]
+        public void Transpose(NoteName noteName, int halfSteps, NoteName expectedNoteName)
+        {
+            var interval = Interval.FromHalfSteps(halfSteps);
+            var actualNoteName = noteName.Transpose(interval);
+            Assert.AreEqual(expectedNoteName, actualNoteName, "Transposed note name is invalid.");
+        }
+
+        #endregion
+    }
+}
