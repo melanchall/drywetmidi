dea8b44155d784ad445f20fa6712276759693b1f
Muiris Woulfe <mw.github@outlook.com>
2019-11-19 19:51:41 +0000

    Exposing NoteId and NoteIdUtilities; Fixing NoteId.Equals bug (#50)
    * Exposing NodeId and NodeItUtilities classes
    
    * Addressing PR feedback and adding unit tests
    
    * Addressing PR feedback on unit tests

--- DryWetMidi/Interaction/Utilities/NoteId/NoteId.cs
+++ DryWetMidi/Interaction/Utilities/NoteId/NoteId.cs
@@ -2,10 +2,18 @@
 
 namespace Melanchall.DryWetMidi.Interaction
 {
-    internal sealed class NoteId
+    /// <summary>
+    /// A class representing the ID of a musical note.
+    /// </summary>
+    public sealed class NoteId
     {
         #region Constructor
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoteId"/> class.
+        /// </summary>
+        /// <param name="channel">The audio channel associated with the musical note.</param>
+        /// <param name="noteNumber">The identification number associated with the musical note.</param>
         public NoteId(FourBitNumber channel, SevenBitNumber noteNumber)
         {
             Channel = channel;
@@ -16,27 +24,45 @@ namespace Melanchall.DryWetMidi.Interaction
 
         #region Properties
 
+        /// <summary>
+        /// Gets the audio channel associated with the musical note.
+        /// </summary>
         public FourBitNumber Channel { get; }
 
+        /// <summary>
+        /// Gets the identification number associated with the musical note.
+        /// </summary>
         public SevenBitNumber NoteNumber { get; }
 
         #endregion
 
         #region Overrides
 
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object against which to compare the current object.</param>
+        /// <returns><c>true</c> if the specified object is equal to the current object; <c>false</c> otherwise.</returns>
         public override bool Equals(object obj)
         {
+            if (ReferenceEquals(obj, null))
+                return false;
+
             if (ReferenceEquals(obj, this))
                 return true;
 
             var noteId = obj as NoteId;
-            if (ReferenceEquals(obj, null))
+            if (ReferenceEquals(noteId, null))
                 return false;
 
             return Channel == noteId.Channel &&
                    NoteNumber == noteId.NoteNumber;
         }
 
+        /// <summary>
+        /// Gets a hash code for the current object.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
         public override int GetHashCode()
         {
             unchecked
