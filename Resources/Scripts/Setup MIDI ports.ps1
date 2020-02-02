8027fa23eb0ebc54c1270803ca156cc408d5e8a0
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2020-02-02 03:32:33 +0300

    Updated CI configs and scripts

--- Resources/Scripts/Setup MIDI ports.ps1
+++ Resources/Scripts/Setup MIDI ports.ps1
@@ -9,23 +9,29 @@ Write-Host "Installing loopMIDI..."
 Start-Process ../Tools/loopMIDISetup.exe -NoNewWindow -Wait -ArgumentList "/quiet"
 $runPath = (Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -Name "loopMIDI").loopMIDI
 
+Write-Host "Waiting 5 seconds before running loopMIDI..."
+sleep 5
+
 Write-Host "Running loopMIDI..."
 & $runPath
-sleep 1
+sleep 3
 
 Write-Host "Destroying existing MIDI ports..."
 $wshell.AppActivate('loopMIDI') | Out-Null
-sleep 1
+sleep 3
 $wshell.SendKeys('+{TAB}+{TAB}{ENTER}{ENTER}')
 Stop-Process -Name "loopMIDI"
 
+Write-Host "Waiting 5 seconds before rerunning loopMIDI..."
+sleep 5
+
 Write-Host "Rerunning loopMIDI..."
 & $runPath
-sleep 1
+sleep 3
 
 Write-Host "Creating MIDI ports..."
 $wshell.AppActivate('loopMIDI') | Out-Null
-sleep 1
+sleep 3
 $wshell.SendKeys('{TAB}MIDI A{TAB}{TAB}{TAB}{ENTER}{TAB}{TAB}MIDI B{TAB}{TAB}{TAB}{ENTER}{TAB}{TAB}MIDI C{TAB}{TAB}{TAB}{ENTER}')
 
 Write-Host "Switching back to '$currentDirectory'..."
