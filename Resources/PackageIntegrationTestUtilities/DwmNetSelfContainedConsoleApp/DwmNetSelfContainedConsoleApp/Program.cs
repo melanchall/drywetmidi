2e772f00aaab03d6ae88d0f2e86b55136dd2f35e
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2021-06-22 13:12:58 +0300

    [Tests][CI] Added package integration tests

---
+++ Resources/PackageIntegrationTestUtilities/DwmNetSelfContainedConsoleApp/DwmNetSelfContainedConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Devices;
+using Melanchall.DryWetMidi.Interaction;
+
+namespace DwmNetSelfContainedConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Is 64-bit operating system: {Environment.Is64BitOperatingSystem}");
+            Console.WriteLine($"Is 64-bit process: {Environment.Is64BitProcess}");
+            Console.WriteLine($"OS version: {Environment.OSVersion}");
+            Console.WriteLine($"CLR version: {Environment.Version}");
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine("Playing MIDI data...");
+
+            var eventsToPlay = new MidiEvent[]
+            {
+                new NoteOnEvent((SevenBitNumber)70, (SevenBitNumber)100),
+                new NoteOffEvent((SevenBitNumber)70, (SevenBitNumber)0) { DeltaTime = 100 }
+            };
+
+            using (var outputDevice = OutputDevice.GetByName("MIDI A"))
+            using (var playback = eventsToPlay.GetPlayback(TempoMap.Default, outputDevice))
+            {
+                playback.EventPlayed += OnEventPlayed;
+                playback.Start();
+
+                SpinWait.SpinUntil(() => !playback.IsRunning);
+            }
+
+            Console.WriteLine("Played.");
+        }
+
+        private static void OnEventPlayed(object sender, MidiEventPlayedEventArgs e)
+        {
+            Console.WriteLine($"Event played: {e.Event}");
+        }
+    }
+}
