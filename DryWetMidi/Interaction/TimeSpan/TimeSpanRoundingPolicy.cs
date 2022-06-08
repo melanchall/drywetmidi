75cdbb9f2c31470364065d17427cbc9b29e6ee63
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-06-08 20:37:13 +0300

    [Docs] Improved docs on TimeSpanRoundingPolicy

--- DryWetMidi/Interaction/TimeSpan/TimeSpanRoundingPolicy.cs
+++ DryWetMidi/Interaction/TimeSpan/TimeSpanRoundingPolicy.cs
@@ -3,8 +3,69 @@
 namespace Melanchall.DryWetMidi.Interaction
 {
     /// <summary>
-    /// Specifies how a time span should be round.
+    /// Specifies how a time span should be rounded.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The default value of the enum is <see cref="NoRounding"/>. Let's see how remaining options work.
+    /// Please note that rounding step should be specified (see
+    /// <see cref="TimeSpanUtilities.Round(ITimeSpan, TimeSpanRoundingPolicy, long, ITimeSpan, TempoMap)"/>).
+    /// </para>
+    /// <para>
+    /// <see cref="RoundUp"/> used to round a time span to the smallest one that is greater than or equal
+    /// to the time span. Following table shows how time span <see href="xref:a_ts_metric">0:0:10</see> will be
+    /// rounded using different steps:
+    /// </para>
+    /// <para>
+    /// <list type="table">
+    /// <listheader>
+    /// <term>Step</term>
+    /// <term>Result</term>
+    /// </listheader>
+    /// <item>
+    /// <term>0:0:15</term>
+    /// <term>0:0:15</term>
+    /// </item>
+    /// <item>
+    /// <term>0:0:10</term>
+    /// <term>0:0:10</term>
+    /// </item>
+    /// <item>
+    /// <term>0:0:3</term>
+    /// <term>0:0:12</term>
+    /// </item>
+    /// </list>
+    /// </para>
+    /// <para>
+    /// <see cref="RoundDown"/> used to round a time span to the largest one that is less than or equal
+    /// to the time span. Following table shows how time span <see href="xref:a_ts_metric">0:0:10</see> will be
+    /// rounded using different steps:
+    /// </para>
+    /// <para>
+    /// <list type="table">
+    /// <listheader>
+    /// <term>Step</term>
+    /// <term>Result</term>
+    /// </listheader>
+    /// <item>
+    /// <term>0:0:3</term>
+    /// <term>0:0:9</term>
+    /// </item>
+    /// <item>
+    /// <term>0:0:10</term>
+    /// <term>0:0:10</term>
+    /// </item>
+    /// <item>
+    /// <term>0:0:6</term>
+    /// <term>0:0:6</term>
+    /// </item>
+    /// <item>
+    /// <term>0:0:15</term>
+    /// <term>0:0:0</term>
+    /// </item>
+    /// </list>
+    /// </para>
+    /// </remarks>
     public enum TimeSpanRoundingPolicy
     {
         /// <summary>
