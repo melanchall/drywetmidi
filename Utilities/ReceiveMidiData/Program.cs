3aa8442f2f7b8712f15d386bb38d0e1a25a448c6
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2023-04-25 20:21:23 +0300

    Add ability to listen to all input devices in ReceiveMidiData

--- Utilities/ReceiveMidiData/Program.cs
+++ Utilities/ReceiveMidiData/Program.cs
@@ -1,11 +1,17 @@
 ﻿using Melanchall.Common;
 using Melanchall.DryWetMidi.Multimedia;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Melanchall.ReceiveMidiData
 {
     internal class Program
     {
+        private const string AllDevicesName = "ALL DEVICES";
+
+        private static bool _listenToAllDevices = false;
+
         static void Main(string[] args)
         {
             UiUtilities.WriteHello();
@@ -17,31 +23,75 @@ input MIDI device and immediately prints the data
             UiUtilities.WriteLine("Here the list of all input MIDI devices in the system:");
             UiUtilities.WriteLine();
 
-            var inputDevices = InputDevice.GetAll();
-            UiUtilities.WriteNumberedList(inputDevices, d => d.Name);
+            var inputDevices = InputDevice.GetAll().Concat(new InputDevice[] { null }).ToArray();
+            UiUtilities.WriteNumberedList(inputDevices, d => d?.Name ?? AllDevicesName);
             UiUtilities.WriteLine();
 
             var inputDevice = UiUtilities.SelectElementByNumber("Select device to listen data from (type number)", inputDevices);
+            _listenToAllDevices = inputDevice == null;
             UiUtilities.WriteLine();
 
-            UiUtilities.WriteLine($"Selected device: {inputDevice.Name}");
+            UiUtilities.WriteLine($"Selected device: {inputDevice?.Name ?? AllDevicesName}");
             UiUtilities.WriteLine("Starting listening MIDI data...");
-            inputDevice.EventReceived += OnEventReceived;
-            inputDevice.StartEventsListening();
+
+            if (_listenToAllDevices)
+                StartEventsListeningOnAllDevices(inputDevices);
+            else
+                StartEventsListeningOnSpecificDevice(inputDevice);
+
             UiUtilities.WriteLine("Listening... Press Esc to stop the utility");
             UiUtilities.WriteLine();
 
             UiUtilities.WaitForOneOfKeys(ConsoleKey.Escape);
 
             UiUtilities.WriteLine("Releasing the device...");
+
+            if (_listenToAllDevices)
+                StopEventsListeningOnAllDevices(inputDevices);
+            else
+                StopEventsListeningOnSpecificDevice(inputDevice);
+
+            UiUtilities.WriteLine("Exited.");
+        }
+
+        private static void StartEventsListeningOnSpecificDevice(InputDevice inputDevice)
+        {
+            inputDevice.EventReceived += OnEventReceived;
+            inputDevice.StartEventsListening();
+        }
+
+        private static void StopEventsListeningOnSpecificDevice(InputDevice inputDevice)
+        {
             inputDevice.EventReceived -= OnEventReceived;
             inputDevice.Dispose();
-            UiUtilities.WriteLine("Exited.");
+        }
+
+        private static void StartEventsListeningOnAllDevices(ICollection<InputDevice> inputDevices)
+        {
+            foreach (var inputDevice in inputDevices)
+            {
+                if (inputDevice == null)
+                    continue;
+
+                StartEventsListeningOnSpecificDevice(inputDevice);
+            }
+        }
+
+        private static void StopEventsListeningOnAllDevices(ICollection<InputDevice> inputDevices)
+        {
+            foreach (var inputDevice in inputDevices)
+            {
+                if (inputDevice == null)
+                    continue;
+
+                StopEventsListeningOnSpecificDevice(inputDevice);
+            }
         }
 
         private static void OnEventReceived(object sender, MidiEventReceivedEventArgs e)
         {
-            UiUtilities.WriteLine(e.Event.ToString());
+            var deviceName = _listenToAllDevices ? $"{((InputDevice)sender).Name}: " : string.Empty;
+            UiUtilities.WriteLine($"{deviceName}{e.Event}");
         }
     }
 }
