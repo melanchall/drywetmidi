996117708eeeed2a92641a7114a198d6134e886b
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2026-02-01 21:38:34 +0300

    Linux support

--- Resources/Scripts/Copy native binaries locally.ps1
+++ Resources/Scripts/Copy native binaries locally.ps1
@@ -16,3 +16,8 @@ Write-Host "Copying x64/arm64 dylib..."
 $macosX64Arm64Destination = "Native\macos_x64_arm64"
 New-Item -ItemType Directory -Force -Path $macosX64Arm64Destination
 Copy-Item -Path "$fromDirectory/Native_macOS_x64_arm64/Melanchall_DryWetMidi_Native.dylib" -Destination "$macosX64Arm64Destination" -Force -Verbose
+
+Write-Host "Copying x64 so..."
+$linuxX64Destination = "Native\linux_x64"
+New-Item -ItemType Directory -Force -Path $linuxX64Destination
+Copy-Item -Path "$fromDirectory/Native_Linux_x64/Melanchall_DryWetMidi_Native.so" -Destination "$linuxX64Destination" -Force -Verbose
