08c5e5016a5080270e2b7412e7d8fac39e13bece
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2026-08-20 23:00:03 +0300

    Bench

---
+++ DryWetMidi.Benchmarks/GetFileNotesBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+
+namespace Melanchall.DryWetMidi.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class GetFileNotesBenchmarks
+    {
+        private MidiFile _midiFile = null!;
+
+        [ParamsSource(nameof(FilesPaths))]
+        public string FilePath { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _midiFile = MidiFile.Read(FilePath);
+        }
+
+        [Benchmark]
+        public int GetNotes()
+        {
+            var notes = _midiFile.GetNotes();
+            return notes.Count;
+        }
+
+        public string[] FilesPaths => Directory
+            .GetFiles(@"D:\Dropbox\Melanchall\DryWetMIDI\drywetmidi\Resources\MIDI files\Valid", "*.*", SearchOption.AllDirectories)
+            .ToArray();
+    }
+}
